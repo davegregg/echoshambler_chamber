c28394c87732189651ffc1614f39eb23738eb6f4
davegregg <davegregg@gmail.com>
2017-03-22 21:28:41 -0400

    Error handling up.

--- main.js
+++ main.js
@@ -2,14 +2,20 @@ $(document).ready(function(){
 
   var source   = $("#grunts-template").html()
   var template = Handlebars.compile(source)
+  var errors_source   = $("#errors-template").html()
+  var errors_template = Handlebars.compile(errors_source)
 
   Handlebars.registerHelper("howlongago", (date) => {
     return $.timeago(date)
   })
 
-  $.get({ url: 'https://echoshambler.herokuapp.com/grunts', data_type: 'json' })
-   .then((response) => {
+  $.ajax({url: 'https://echoshambler.herokuapp.com/grunts', data_type: 'json', method: 'post'})
+   .done((response) => {
      $("#grunts").html(template(response))
+   })
+   .fail(function(code, textStatus, xhr) {
+     error = { code: code, message: xhr}
+     $("#grunts").html(errors_template(error))
    })
 
 })
