0ba3b23870362f7cd1b8a3f86a7e2bc236a17cdb
davegregg <davegregg@gmail.com>
2017-03-22 21:45:23 -0400

    Minor error handling textual change

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  var source   = $("#grunts-template").html()
-  var template = Handlebars.compile(source)
-  var errors_source   = $("#errors-template").html()
-  var errors_template = Handlebars.compile(errors_source)
+    var source = $("#grunts-template").html()
+    var template = Handlebars.compile(source)
+    var errors_source = $("#errors-template").html()
+    var errors_template = Handlebars.compile(errors_source)
 
-  Handlebars.registerHelper("howlongago", (date) => {
-    return $.timeago(date)
-  })
+    Handlebars.registerHelper("howlongago", (date) => {
+        return $.timeago(date)
+    })
 
-  $.ajax({url: 'https://echoshambler.herokuapp.com/grunts', data_type: 'json'})
-   .done((response) => {
-     $("#grunts").html(template(response))
-   })
-   .fail(function(code, textStatus, xhr) {
-     error = { code: code, message: xhr}
-     $("#grunts").html(errors_template(error))
-   })
-
+    $.get({
+            url: 'https://echoshambler.herokuapp.com/grunts',
+            data_type: 'json'
+        })
+        .done((response) => {
+            $("#grunts").html(template(response))
+        })
+        .fail((status, textStatus, xhr) => {
+            error = {
+                code: status,
+                textStatus: textStatus,
+                message: xhr
+            }
+            $("#grunts").html(errors_template(error))
+        })
 })
